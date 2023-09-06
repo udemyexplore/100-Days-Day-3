0f3d3296f154e0fb47a1736b5a4f82d9980de673
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-09-06 06:24:01 +0300

    feat:learning

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm"))
+
+
+if height > 180:
+    print("You can't ride this rollercoaster!")
+else: 
+    print("You can ride the rollercoaster.")   
